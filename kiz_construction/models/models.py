b1afb93bd607d32b5684c223785ed9ac89c435f9
江口和志 <sandwhale1010@gmail.com>
2022-04-01 11:17:42 +0900

    明細コピー追加

--- kiz_construction/models/models.py
+++ kiz_construction/models/models.py
@@ -28,42 +28,42 @@ class kiz_construction(models.Model):
         "ships.ship",
         string="ship"
     )  # S番
-    name = fields.Char(string="name")
-    production_name = fields.Char(string="Production Name")
+    name = fields.Char(string="name")   # 品名
+    production_name = fields.Char(string="Production Name")     # 制作名称
     first_category = fields.Char(string="First category")
     second_category = fields.Char(string="Second category")
     ship_class = fields.Char(string="Ship class")
     drawing_number = fields.Char(string="drawing number")
-    painting = fields.Char(string="painting")
+    painting = fields.Char(string="painting")  # 塗装
     product_number = fields.Char(string="product number")
-    date_of_issue = fields.Datetime(string="date of issue")
-    finished_making_the_day = fields.Date(string="Finished making the day")
-    designer = fields.Char(string="Designer")
-    machining_drawing = fields.Char(string="Machining drawing")
-    in_house_construction_drawing = fields.Char(string="In-house construction drawing")
+    date_of_issue = fields.Datetime(string="date of issue")  # 製作管理票発行日
+    finished_making_the_day = fields.Date(string="Finished making the day")     # 製缶完了日
+    designer = fields.Char(string="Designer")   # 設計担当者
+    machining_drawing = fields.Char(string="Machining drawing") # 加工図
+    in_house_construction_drawing = fields.Char(string="In-house construction drawing")  # 社内工事図
     attachment_id = fields.Many2one('ir.attachment', string="Attachment")
-    isomorphism = fields.Char(string="Isomorphism")
-    items = fields.Char(string="items")
-    note = fields.Text(string="note")
-    design_note = fields.Char(string="Design note")
-    nesting_notes = fields.Char(string="Nesting Notes")
-    material_input_person = fields.Char(string="Material input person")
-    production_place = fields.Char(string="Production place")
+    isomorphism = fields.Char(string="Isomorphism")  # 同型
+    items = fields.Char(string="items")  # 内訳
+    note = fields.Text(string="note")   # 備考
+    design_note = fields.Char(string="Design note")  # 設計備考
+    nesting_notes = fields.Char(string="Nesting Notes")  # ネスティング備考
+    material_input_person = fields.Char(string="Material input person")  # 資材入力者
+    production_place = fields.Char(string="Production place")   # 製作場所
     correction_area = fields.Char(string="correction area")
     china_arrival_date = fields.Date(string="China arrival date")
-    procurement_date = fields.Date(string="Procurement date")
-    departure_date = fields.Date(string="Departure date")
+    procurement_date = fields.Date(string="Procurement date")  # 調達日
+    departure_date = fields.Date(string="Departure date")  # 出港日
     in_days = fields.Integer(string="in days")
     in_date = fields.Datetime(string="in date")
     china_days = fields.Integer(string="china days")
     china_date = fields.Date(string="china date")
-    consultation_drawing = fields.Many2one('ir.attachment', string="Consultation drawing")
+    consultation_drawing = fields.Many2one('ir.attachment', string="Consultation drawing")  # 協議図
     approved_drawing = fields.Many2one('ir.attachment', string="Approved drawing")
     construction_drawing = fields.Many2one('ir.attachment', string="Construction drawing")
     steel_material_order_date = fields.Date(string="Steel material order date")
     steel_material_arrangement_date = fields.Date(string="Steel material arrangement date")
-    paint_order_date = fields.Date(string="paint order date")
-    paint_arrangement_date = fields.Date(string="paint arrangement date")
+    paint_order_date = fields.Date(string="paint order date")    # 塗装注文日
+    paint_arrangement_date = fields.Date(string="paint arrangement date")    # 塗装手配日
     gross_weight = fields.Float(string="Gross weight")
     expected_gross_weight = fields.Float(string="Expected gross weight", related='s_no.total_weight')
     const_files = fields.One2many(
