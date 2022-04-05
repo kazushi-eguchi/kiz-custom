dd80f6a51e8b92b0efa581c68a6bdbbfef4ff295
江口和志 <sandwhale1010@gmail.com>
2022-04-06 08:56:28 +0900

    カスタム値表示調査中

--- kiz_purchase_request/models/__init__.py
+++ kiz_purchase_request/models/__init__.py
@@ -3,4 +3,4 @@
 # from . import purchase_request_form
 # from . import purchase_order
 # from . import purchase_request_line_make_purchase_order
-
+from . import  purchase_order_line
---
+++ product_configurator_configids/models/purchase_order_line.py
@@ -0,0 +1,14 @@
+# # -*- coding: utf-8 -*-
+#
+from odoo import models, fields, api
+
+class KizPurchaseOrderLines(models.Model):
+    _inherit = 'purchase.order.line'
+
+    custom_values = fields.Many2one(
+        string="custom_values",
+        comodel_name="product.config.session.custom.value",
+        readonly=True,
+        related="config_session_id.value",
+        store=True,
+    )
