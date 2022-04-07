fec0f46bddbd5433e804272d5a3f072411667fdd
江口和志 <sandwhale1010@gmail.com>
2022-04-07 11:54:07 +0900

    カスタム値表示

--- product_configurator_configids/models/purchase_order_line.py
+++ product_configurator_configids/models/purchase_order_line.py
@@ -5,10 +5,11 @@ from odoo import models, fields, api
 class KizPurchaseOrderLines(models.Model):
     _inherit = 'purchase.order.line'
 
-    custom_values = fields.Many2one(
-        string="custom_values",
-        comodel_name="product.config.session.custom.value",
-        readonly=True,
-        related="config_session_id.value",
-        store=True,
-    )
+    custom_values = fields.Char(compute="_compute_custom_values")
+
+    def _compute_custom_values(self):
+        for line in self:
+            vals = []
+            for custom_value in line.custom_value_ids:
+                vals.append(custom_value.display_name + ": " + custom_value.value)
+            line.custom_values = ",".join(vals)
