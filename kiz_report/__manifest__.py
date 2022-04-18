d4f6ca3bae74946d9647bf54bf2f07642e129062
江口和志 <sandwhale1010@gmail.com>
2022-04-18 17:54:46 +0900

    レポート調整

--- kiz_report/__manifest__.py
+++ kiz_report/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'purchase_request', 'kiz_purchase_request'],
+    'depends': ['base', 'purchase_request', 'kiz_purchase_request', 'kiz_construction'],
 
     # always loaded
     'data': [
