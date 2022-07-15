13b25bfb4ecbe44d6e47850ceeaac753fad4c697
江口和志 <sandwhale1010@gmail.com>
2022-07-15 16:51:28 +0900

    属性表示名更新準備

--- kiz_report/models/product_attribute.py
+++ kiz_report/models/product_attribute.py
@@ -5,5 +5,7 @@ from odoo import models, fields, api
 
 class ProductAttributeExt(models.Model):
     _inherit = 'product.attribute'
+    _rec_name = 'name'
 
     display_name = fields.Char(string="表示名")
+    product_name = fields.Char(string="表示名")
