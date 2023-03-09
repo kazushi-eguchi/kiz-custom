36f6b72e4a1798a543b4655efa07f7f332c232a5
江口和志 <sandwhale1010@gmail.com>
2023-03-09 10:21:07 +0900

    納品場所に順序追加

--- kiz_construction/models/delivery_spot.py
+++ kiz_construction/models/delivery_spot.py
@@ -6,7 +6,8 @@ from odoo import models, fields, api
 class ConstFiles(models.Model):
     _name = "purchase.delivery_spot"
     _description = "Delivery Spot"
-    _order = "name"
+    _order = "sequence"
 
     name = fields.Char("name")
+    sequence = fields.Integer(default=1)
     # active = fields.Boolean()
