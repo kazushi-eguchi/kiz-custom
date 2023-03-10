a76888fea90d580283f1ab6c030c037485c47877
江口和志 <sandwhale1010@gmail.com>
2023-03-10 11:43:14 +0900

    順番で確認済みはずれないよう更新

--- kiz_check_product/models/product_configurator.py
+++ kiz_check_product/models/product_configurator.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-
+import logging
 import datetime
 from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
 
 
 class product_configurator_check(models.Model):
@@ -16,10 +18,12 @@ class product_configurator_check(models.Model):
 
     def write(self, values):
         flg = self._origin.check
+        print(values)
+        # _logger.debug('Create a %s with vals %s', self._name, values)
         if self._origin.check == 'no' and values.get("check") == 'yes':
             values['check'] = 'yes'
         else:
-            if 'taxes_id' in values or 'image_1920' in values:
+            if 'taxes_id' in values or 'image_1920' in values or 'sequence' in values:
                 values['check'] = 'yes'
             else:
                 values['check'] = 'no'
